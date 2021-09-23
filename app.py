723be44a7c781153ec3b9a1a29d487a35f63926d
Dattatreya Mohapatra <chikumhptr@gmail.com>
2021-09-22 23:30:50 -0500

    sample db insert code

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import mysql.connector
+from signal import SIGINT, signal
+
+app = Flask(__name__)
+cnx = mysql.connector.connect(user='dm42', host='localhost', database='dm42_database')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        title = details['title']
+        length = details['length']
+        genre = details['genre']
+        cur = cnx.cursor()
+        cur.execute("INSERT INTO MyMovies(title, length, genre) VALUES (%s, %s, %s)", (title, length, genre))
+        cnx.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+
+def sigint_handler(signal_received, frame):
+    global cnx
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    cnx.close()
+    exit(0)
+
+
+if __name__ == '__main__':
+    signal(SIGINT, sigint_handler)
+    app.run()
